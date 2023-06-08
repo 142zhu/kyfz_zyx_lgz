8e7163e7ae4d92b6e315b769d23f3709e39b19b3
ZYX <1322561244@qq.com>
2023-06-08 23:43:46 +0800

    zyx_6.8_修改

--- ruoyi-ui/src/views/kyfz/requirement/index.vue
+++ ruoyi-ui/src/views/kyfz/requirement/index.vue
@@ -8,7 +8,7 @@
       v-show="showSearch"
       label-width="68px"
     >
-      <el-form-item label="类型" prop="projectType">
+      <el-form-item label="项目类型" prop="projectType">
         <el-input
           v-model="queryParams.projectType"
           placeholder="请输入项目类型"
@@ -16,10 +16,10 @@
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="状态" prop="requirementStatus">
+      <el-form-item label="需求状态" prop="requirementStatus">
         <el-input
-          v-model="queryParams.enterpriseNumber"
-          placeholder="请输入项目状态"
+          v-model="queryParams.requirementStatus"
+          placeholder="请输入需求状态"
           clearable
           @keyup.enter.native="handleQuery"
         />
@@ -45,18 +45,6 @@
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['kyfz:requirement:edit']"
-          >修改</el-button
-        >
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
           type="danger"
           plain
           icon="el-icon-delete"
@@ -65,17 +53,6 @@
           @click="handleDelete"
           v-hasPermi="['kyfz:requirement:remove']"
           >删除</el-button
-        >
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="el-icon-download"
-          size="mini"
-          @click="handleExport"
-          v-hasPermi="['kyfz:requirement:export']"
-          >导出</el-button
         >
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
@@ -153,7 +130,7 @@
         <el-form-item label="需求名称" prop="projectName">
           <el-input
             v-model="form.projectName"
-            placeholder="NULL"
+            placeholder=""
             :readonly="editable"
             ref="idInput"
             style="width: 600px"
@@ -162,7 +139,7 @@
         <el-form-item label="需求关键词" prop="requirementKeywords">
           <el-input
             v-model="form.requirementKeywords"
-            placeholder="NULL"
+            placeholder=""
             :readonly="editable"
             ref="idInput"
             style="width: 600px"
@@ -171,7 +148,7 @@
         <el-form-item label="需求描述" prop="requirementDescription">
           <el-input
             v-model="form.requirementDescription"
-            placeholder="NULL"
+            placeholder=""
             :readonly="editable"
             ref="idInput"
             style="width: 600px"
@@ -182,7 +159,7 @@
         <el-form-item label="需求类型" prop="projectCategory">
           <el-input
             v-model="form.projectCategory"
-            placeholder="NULL"
+            placeholder=""
             :readonly="editable"
             ref="idInput"
             style="width: 600px"
@@ -194,7 +171,7 @@
             v-model="form.requirementReleaseTime"
             type="date"
             value-format="yyyy-MM-dd"
-            placeholder="NULL"
+            placeholder=""
             :readonly="true"
             ref="idInput"
             style="width: 600px"
@@ -205,7 +182,7 @@
           <el-select
             v-model="form.requirementStatus"
             filterable
-            placeholder="请选择需求状态"
+            placeholder=""
             :disabled="editable"
             style="width: 600px"
           >
@@ -222,7 +199,7 @@
           <el-select
             v-model="form.enterpriseName"
             filterable
-            placeholder="请选择已有公司"
+            placeholder=""
             :disabled="editable"
             style="width: 600px"
             @change="handleChange"
@@ -240,7 +217,7 @@
           <el-input
             v-model="form.enterpriseDescribe"
             type="textarea"
-            placeholder="NULL"
+            placeholder=""
             :readonly="true"
             ref="idInput"
             style="width: 600px"
@@ -250,7 +227,7 @@
         <el-form-item label="注册资本" prop="registeredCapital" class="inputDeep">
           <el-input
             v-model="form.registeredCapital"
-            placeholder="NULL"
+            placeholder=""
             :readonly="true"
             ref="idInput"
             style="width: 600px"
@@ -258,6 +235,99 @@
         </el-form-item>
         <el-form-item label="公司编号" prop="enterpriseNumber" hidden>
           <el-input v-model="form.enterpriseNumber" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 上传需求管理对话框 -->
+    <el-dialog :title="title" :visible.sync="open1" width="1000px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="250px">
+        <el-form-item label="需求名称" prop="projectName">
+          <el-input
+            v-model="form.projectName"
+            placeholder="请输入需求名称"
+            style="width: 600px"
+          />
+        </el-form-item>
+        <el-form-item label="需求关键词" prop="requirementKeywords">
+          <el-input
+            v-model="form.requirementKeywords"
+            placeholder="请输入需求关键词"
+            style="width: 600px"
+          />
+        </el-form-item>
+        <el-form-item label="需求描述" prop="requirementDescription">
+          <el-input
+            v-model="form.requirementDescription"
+            placeholder="请输入需求描述"
+            style="width: 600px"
+            type="textarea"
+            :autosize="{ minRows: 2, maxRows: 4 }"
+          />
+        </el-form-item>
+        <el-form-item label="需求类型" prop="projectCategory">
+          <el-input
+            v-model="form.projectCategory"
+            placeholder="请输入需求类型"
+            style="width: 600px"
+          />
+        </el-form-item>
+        <el-form-item label="需求状态" prop="requirementStatus">
+          <el-select
+            v-model="form.requirementStatus"
+            filterable
+            placeholder="请选择需求状态"
+            style="width: 600px"
+          >
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="公司名称" prop="enterpriseName">
+          <el-select
+            v-model="form.enterpriseName"
+            filterable
+            placeholder="请选择已有公司名称"
+            style="width: 600px"
+            @change="handleChange"
+          >
+            <el-option
+              v-for="item in enterpriseList"
+              :key="item.enterpriseName"
+              :label="item.enterpriseName"
+              :value="item.enterpriseName"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="公司简介" prop="enterpriseDescribe">
+          <el-input
+            v-model="form.enterpriseDescribe"
+            type="textarea"
+            placeholder=""
+            :readonly="true"
+            ref="idInput"
+            style="width: 600px"
+            :autosize="{ minRows: 2, maxRows: 4 }"
+          />
+        </el-form-item>
+        <el-form-item label="注册资本" prop="registeredCapital">
+          <el-input
+            v-model="form.registeredCapital"
+            placeholder=""
+            :readonly="true"
+            ref="idInput"
+            style="width: 600px"
+          />
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -301,6 +371,7 @@ export default {
       title: "",
       // 是否显示弹出层
       open: false,
+      open1: false,
       // 查询参数
       queryParams: {
         pageNum: 1,
@@ -457,7 +528,7 @@ export default {
     /** 新增按钮操作 */
     handleAdd() {
       this.reset();
-      this.open = true;
+      this.open1 = true;
       this.title = "上传需求管理";
     },
     /** 修改按钮操作 */
@@ -483,7 +554,7 @@ export default {
           } else {
             addRequirement(this.form).then((response) => {
               this.$modal.msgSuccess("新增成功");
-              this.open = false;
+              this.open1 = false;
               this.getList();
             });
           }
