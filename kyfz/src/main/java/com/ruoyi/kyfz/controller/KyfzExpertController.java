38bb1db83228efe7fe0dd323ad32a20fc46e6e1a
yanping <my.yanp@gmail.com>
2023-06-16 10:30:50 +0800

    移除:专家列表详情按钮权限控制

--- kyfz/src/main/java/com/ruoyi/kyfz/controller/KyfzExpertController.java
+++ kyfz/src/main/java/com/ruoyi/kyfz/controller/KyfzExpertController.java
@@ -1,31 +1,25 @@
 package com.ruoyi.kyfz.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.kyfz.domain.KyfzExpert;
 import com.ruoyi.kyfz.service.IKyfzExpertService;
 import com.ruoyi.kyfz.service.IKyfzMatchService;
-import com.ruoyi.common.utils.poi.ExcelUtil;
-import com.ruoyi.common.core.page.TableDataInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 专家管理Controller
- * 
+ *
  * @author ruoyi
  * @date 2023-06-06
  */
@@ -103,7 +97,7 @@ public class KyfzExpertController extends BaseController {
     /**
      * 获取专家详细信息
      */
-    @PreAuthorize("@ss.hasPermi('kyfz:expert:detail')")
+//    @PreAuthorize("@ss.hasPermi('kyfz:expert:detail')")
     @GetMapping(value = "/detail/{expertId}")
     public AjaxResult getDetailInfo(@PathVariable("expertId") Long expertId) {
         KyfzExpert expert = kyfzExpertService.selectKyfzExpertByExpertId(expertId);
